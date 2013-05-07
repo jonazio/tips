709b1abf04ed1149d29355d4474252e21dd78230
Jonas Ahsin <jonas.ahsin@kentor.se>
2013-05-07 15:48:59 +0200

    Started with file upload.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,14 @@
 package controllers;
 
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+
 import models.Tipsrow;
 import play.*;
 import play.mvc.*;
@@ -10,6 +19,26 @@ public class Application extends Controller {
   
     public static Result index() {
         return ok(index.render("0"));
+    }
+
+    public static Result upload() throws FileNotFoundException {
+        MultipartFormData body = request().body().asMultipartFormData();
+        FilePart tips = body.getFile("tips");
+        if (tips != null) {
+            // String fileName = tips.getFilename();
+            // String contentType = tips.getContentType(); 
+            File file = tips.getFile();
+            Scanner sc = new Scanner(file);
+            String output = sc.nextLine();
+            output = sc.nextLine();
+            sc.close();
+            return ok(output);
+        } else {
+            flash("error", "Missing file");
+            return redirect(routes.Application.index()); 
+        }
+        
+        // make sure that the uploaded-file is a txt-file
     }
     
     public static Result testoutput() {
